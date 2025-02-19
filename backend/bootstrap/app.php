0503c24c0a2fbf8c409c97ea34ea0a2821d78e53
Mohammad <mohammad.rfn98@gmail.com>
2025-02-19 11:57:21 +0330

    Authentication-set-clear

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
+        
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
